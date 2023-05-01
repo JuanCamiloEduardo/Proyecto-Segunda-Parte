968838c1f2f0d11cf0bb44414786089680ae6e07
j.burbanon <j.burbanon@uniandes.edu.co>
2023-04-30 20:10:31 -0500

    Cambios app

--- app.py
+++ app.py
@@ -11,48 +11,11 @@ import numpy as np
 top_words = 10000
 max_words = 500
 
-#load the csv file saved
-
-df_tracks=pd.read_csv('data/MovieReviewsPruebas.csv', sep=',', encoding = 'utf-8')
-
-"""
-
-tokenizer_obj = Tokenizer(num_words=top_words)
-tokenizer_obj = Tokenizer(num_words=top_words)
-tokenizer_obj.fit_on_texts(df.loc[:50000, 'review'].values)"""
-def label_to_number(label):
-    if label == "positivo":
-        return 1
-    else:
-        return 0
-    
- 
-df_tracks['Clean Review'] = df_tracks['review_es'].apply(clean.clean_text)
-print( df_tracks['review_es'][1])
-df_tracks['Clean Review'] = df_tracks['Clean Review'].apply(clean.remove_stopwords)
-df_tracks['Clean Review'] = df_tracks['Clean Review'].apply(clean.snowball_lemmatize)
-print( df_tracks['Clean Review'][1])
-
-vectorizer = TfidfVectorizer(max_df=1200, min_df=2, max_features=10000, ngram_range=(1,2))
-X_count = vectorizer.fit_transform(df_tracks['Clean Review'])
-print("TErmino lematizacion y vectorzi")
-def pred(usermoviereview):
-    print("TIemne que predeceir")
-    """"
-    review_tokens = tokenizer_obj.texts_to_sequences(test_samples)
-    review_tokens_pad = pad_sequences(review_tokens, maxlen=max_words)
-    
-    """
-
-   
-    
+def pred(usermoviereview): 
     delete = clean.clean_text(usermoviereview)
     stopwords=clean.remove_stopwords(delete)
     review_tokens_pad=clean.tokenize_text(stopwords)
     
-    #vectorizer=TfidfVectorizer()
-    
-    #review_tokens_pad = vectorizer.fit_transform(test)
    
     print("call predict")
     # Load in pretrained model
@@ -70,7 +33,6 @@ def pred(usermoviereview):
 # webapp
 app = Flask(__name__, template_folder='./')
 
-
 @app.route('/prediction', methods=['POST', 'GET'])
 def prediction():
     if request.method == "POST":
@@ -81,10 +43,29 @@ def prediction():
         return jsonify(response)
     return jsonify("Input text")
 
+@app.route('/archivo', methods=['POST', 'GET'])
+def cargar_csv():
+    #if request.method == "POST":
+    df_tracks=pd.read_csv('data/MovieReviewsPruebas.csv', sep=',', encoding = 'utf-8')
+    df_tracks['Clean Review'] = df_tracks['review_es'].apply(clean.clean_text)
+    df_tracks['Clean Review'] = df_tracks['Clean Review'].apply(clean.remove_stopwords)
+    df_tracks['Clean Review'] = df_tracks['Clean Review'].apply(clean.snowball_lemmatize)
+    
+    loaded_model2=joblib.load('pipeline.pkl', 'rb')
+    sentiment2 = loaded_model2.predict(df_tracks['Clean Review'])
+    a=np.array(sentiment2)
+    unique, counts = np.unique(a, return_counts=True)
+    a=dict(zip(unique, counts))
+    size=len(sentiment2)
+    negative=a[0]/size
+    positive=a[1]/size
+    probability="Percentage of positive comments: {:.3f}  Percentage of negative comments: {:.3f}".format(float(positive),float(negative))
+    print(probability)
+    return  jsonify(probability)
+    
 @app.route('/')
 def main():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
